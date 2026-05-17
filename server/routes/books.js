96cb4466b4fa75c1bc4d9fad58968c738aed3c3c
hungrychef-bytescode <hungrychefbytescode@gmail.com>
2026-05-17 18:16:21 +0800

    [GREEN] implement integration layer for CRUD create book operation

--- server/routes/books.js
+++ server/routes/books.js
@@ -1,9 +1,15 @@
 const express = require("express")
 
 const router = express.Router()
+const createBook = require("../services/createBook")
 
 router.post("/", (req, res) => {
-    throw new Error("not yet implemented")
+    try {
+        const newBook = createBook(req.body)
+        res.status(201).json(newBook)
+    } catch (err) {
+        res.status(400).json({ error: err.message })
+    }
 })
 
 module.exports = router
--- server/services/createBook.js
+++ server/services/createBook.js
@@ -1,5 +1,24 @@
-function createBook() {
-  throw new Error("not yet implemented")
+const books = require("../data/books")
+
+const formatBookData = require("../utils/formatBookData")
+const validateBookTitle = require("../utils/validateBookTitle")
+const validateReadingStatus = require("../utils/validateReadingStatus")
+
+function createBook(bookData) {
+    const formattedBook = formatBookData(bookData)
+    
+    if (!validateBookTitle(formattedBook.title)) {
+        throw new Error("Title is required")
+    }
+
+    if (!validateReadingStatus(formattedBook.readingStatus)) {
+        throw new Error("Invalid reading status")
+    }
+  
+    const newBook = { id: books.length + 1, ...formattedBook }
+
+    books.push(newBook)
+    return newBook
 }
 
 module.exports = createBook
