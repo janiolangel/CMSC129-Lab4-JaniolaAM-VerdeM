6b03cb1c474a1b5741823b4441ee9ad050daabbd
hungrychef-bytescode <hungrychefbytescode@gmail.com>
2026-05-17 14:13:14 +0800

    [GREEN] implement book title validation

--- server/utils/validateBookTitle.js
+++ server/utils/validateBookTitle.js
@@ -1,5 +1,13 @@
-function validateBookTitle() {
-  throw new Error("not yet implemented")
+function validateBookTitle(title) {
+    if (!title.trim()) {
+        return false
+    }
+    
+    if (title.length > 500) {
+        return false
+    }
+    
+    return true
 }
 
 module.exports = validateBookTitle
